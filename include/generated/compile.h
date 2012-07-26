f7a3d8f0ca2580d84061a80165db15a43d19fadf
Brian Beloshapka <bbelos@hotmail.com>
2012-07-26 15:31:14 -0500

    LG8 update from opensource.samsung.com

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Tue Jun 19 12:18:06 KST 2012"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jul 23 20:55:22 KST 2012"
 #define LINUX_COMPILE_BY "shina.lee"
 #define LINUX_COMPILE_HOST "Sprint10"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
